0c84eb39c887f02f7fc986a48f5a123f1907e22d
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-10 11:37:00 +0000

    tc15

---
+++ src/test/java/my_interface/consumer/test_1.java
@@ -0,0 +1,14 @@
+package my_interface.consumer;
+
+import org.testng.annotations.Test;
+
+public class test_1 {
+    //no return -> void
+    // onyl accept
+
+
+    @Test
+    public void testName() {
+
+    }
+}
---
+++ src/test/java/my_interface/predicate/test_1.java
@@ -0,0 +1,11 @@
+package my_interface.predicate;
+
+import org.testng.annotations.Test;
+
+public class test_1 {
+
+    // return boolean
+    @Test
+    public void testName() {
+    }
+}
